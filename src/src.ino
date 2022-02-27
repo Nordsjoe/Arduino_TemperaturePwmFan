ae2854f4afaf9bf19850cee5a559db105cbd1b23
Albin Nordsjö <albin.nordsjo@gmail.com>
2022-02-27 22:13:14 +0100

    Add first version of PWM temperature controlled fan

---
+++ src/src.ino
@@ -0,0 +1,119 @@
+
+// PWM code stolen from : https://create.arduino.cc/projecthub/tylerpeppy/25-khz-4-pin-pwm-fan-control-with-arduino-uno-3005a1
+
+const byte OC1A_PIN = 9;
+const byte OC1B_PIN = 10;
+
+const word PWM_FREQ_HZ = 25000; //Adjust this value to adjust the frequency (Frequency in HZ!) (Set currently to 25kHZ)
+const word TCNT1_TOP = 16000000/(2*PWM_FREQ_HZ);
+
+#define MIN_FAN_SPEED 30  // Minimum allowed fan speed (% duty cycle)
+#define MAX_FAN_SPEED 100 // Maxium fan speed (% duty cycle)
+
+// Temperature limits. Between these limits fan speed increases lineraly with temperature
+#define TEMP_MIN 25.0  // Below this temperature fan runs at MIN_FAN_SPEED (deg C)
+#define TEMP_MAX 40.0  // Abobe this temperature fan runs at MAX_FAN_SPEED (deg C)
+
+// States
+#define STATE_MAX_SPEED 0
+#define STATE_MIN_SPEED 1
+#define STATE_LINEAR 2
+
+// Linear constants
+const float k = (MAX_FAN_SPEED - MIN_FAN_SPEED) / (TEMP_MAX - TEMP_MIN);
+const float m = MIN_FAN_SPEED - k * TEMP_MIN;
+
+// Temp sensor code
+const int sensorPin = A0;
+// Temp sensor code end
+
+void setup() {
+  
+  // Temp sensor code start
+  Serial.begin(9600); // Open a serial port
+  // Temp sensor code end
+  
+  pinMode(OC1A_PIN, OUTPUT);
+
+  // Clear Timer1 control and count registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+
+  // Set Timer1 configuration
+  // COM1A(1:0) = 0b10   (Output A clear rising/set falling)
+  // COM1B(1:0) = 0b00   (Output B normal operation)
+  // WGM(13:10) = 0b1010 (Phase correct PWM)
+  // ICNC1      = 0b0    (Input capture noise canceler disabled)
+  // ICES1      = 0b0    (Input capture edge select disabled)
+  // CS(12:10)  = 0b001  (Input clock select = clock/1)
+  
+  TCCR1A |= (1 << COM1A1) | (1 << WGM11);
+  TCCR1B |= (1 << WGM13) | (1 << CS10);
+  ICR1 = TCNT1_TOP;
+
+}
+
+void loop() {
+  static int state = STATE_MAX_SPEED;
+  static byte pwmDuty = 100;
+  // Temp sensor code start
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor value: ");
+  Serial.print(sensorVal);
+
+  // Convert ADC reading to voltage
+  float voltage = (sensorVal/1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+
+  // Convert to degC
+  float temperature = (voltage - .5) * 100;
+  Serial.print(", degrees C: ");
+  Serial.print(temperature);
+  // Temp sensor code end
+
+  if (temperature < TEMP_MIN) 
+  {
+    state = STATE_MIN_SPEED;
+  }
+  else if (temperature > TEMP_MIN && temperature < TEMP_MAX)
+  {
+    state = STATE_LINEAR;
+  }
+  else
+  {
+    // Default state, max speed
+    state = STATE_MAX_SPEED;
+  }
+  
+
+  switch (state) {
+  case STATE_MIN_SPEED:
+    setPwmDuty(MIN_FAN_SPEED);
+    break;
+  case STATE_LINEAR:
+    pwmDuty = calcPwmDuty(temperature);
+    setPwmDuty(pwmDuty);
+    Serial.print(", PWM duty: ");
+    Serial.println(pwmDuty);
+    break;
+  default:
+    // Max speed, temperature above TEMP_MAX or faulty
+    setPwmDuty(MAX_FAN_SPEED);
+    break;
+  }
+
+}
+
+void setPwmDuty(byte duty) {
+  OCR1A = (word) (duty*TCNT1_TOP)/100;
+}
+
+byte calcPwmDuty(float temperature) {
+  // Linear 
+  float pwmDuty_float = k * temperature + m;
+  // Convert to byte
+  byte pwmDuty_byte = (byte)(pwmDuty_float+0.5);
+  return pwmDuty_byte;
+}
