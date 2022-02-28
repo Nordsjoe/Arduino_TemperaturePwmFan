a8211c28b885d4cd59dcb9e90b3e6ff5bd99d4bf
Albin Nordsjö <albin.nordsjo@gmail.com>
2022-02-28 08:12:18 +0100

    Add error handling of calculated temperature and saturate calculated PWM duty cycle.

--- src/src.ino
+++ src/src.ino
@@ -5,7 +5,7 @@ const byte OC1A_PIN = 9;
 const byte OC1B_PIN = 10;
 
 const word PWM_FREQ_HZ = 25000; //Adjust this value to adjust the frequency (Frequency in HZ!) (Set currently to 25kHZ)
-const word TCNT1_TOP = 16000000/(2*PWM_FREQ_HZ);
+const word TCNT1_TOP = 16000000 / (2 * PWM_FREQ_HZ);
 
 #define MIN_FAN_SPEED 30  // Minimum allowed fan speed (% duty cycle)
 #define MAX_FAN_SPEED 100 // Maxium fan speed (% duty cycle)
@@ -28,11 +28,11 @@ const int sensorPin = A0;
 // Temp sensor code end
 
 void setup() {
-  
+
   // Temp sensor code start
   Serial.begin(9600); // Open a serial port
   // Temp sensor code end
-  
+
   pinMode(OC1A_PIN, OUTPUT);
 
   // Clear Timer1 control and count registers
@@ -47,7 +47,7 @@ void setup() {
   // ICNC1      = 0b0    (Input capture noise canceler disabled)
   // ICES1      = 0b0    (Input capture edge select disabled)
   // CS(12:10)  = 0b001  (Input clock select = clock/1)
-  
+
   TCCR1A |= (1 << COM1A1) | (1 << WGM11);
   TCCR1B |= (1 << WGM13) | (1 << CS10);
   ICR1 = TCNT1_TOP;
@@ -63,17 +63,26 @@ void loop() {
   Serial.print(sensorVal);
 
   // Convert ADC reading to voltage
-  float voltage = (sensorVal/1024.0) * 5.0;
+  float voltage = (sensorVal / 1024.0) * 5.0;
   Serial.print(", Volts: ");
   Serial.print(voltage);
 
   // Convert to degC
   float temperature = (voltage - .5) * 100;
+
+  // Do some error handling of the calculated temperature. 
+  // If temperature is negative or temperature > 100 degC the sensor is probably faulty.
+  if (temperature < 0.0 || temperature > 100.0) 
+  {
+    temperature = 100.0;
+    Serial.print("Unexpected temperature detected. Sensor faulty?");
+  }
+  
   Serial.print(", degrees C: ");
   Serial.print(temperature);
   // Temp sensor code end
 
-  if (temperature < TEMP_MIN) 
+  if (temperature < TEMP_MIN)
   {
     state = STATE_MIN_SPEED;
   }
@@ -86,34 +95,40 @@ void loop() {
     // Default state, max speed
     state = STATE_MAX_SPEED;
   }
-  
+
 
   switch (state) {
-  case STATE_MIN_SPEED:
-    setPwmDuty(MIN_FAN_SPEED);
-    break;
-  case STATE_LINEAR:
-    pwmDuty = calcPwmDuty(temperature);
-    setPwmDuty(pwmDuty);
-    Serial.print(", PWM duty: ");
-    Serial.println(pwmDuty);
-    break;
-  default:
-    // Max speed, temperature above TEMP_MAX or faulty
-    setPwmDuty(MAX_FAN_SPEED);
-    break;
+    case STATE_MIN_SPEED:
+      setPwmDuty(MIN_FAN_SPEED);
+      break;
+    case STATE_LINEAR:
+      pwmDuty = calcPwmDuty(temperature);
+      setPwmDuty(pwmDuty);
+      Serial.print(", PWM duty: ");
+      Serial.println(pwmDuty);
+      break;
+    default:
+      // Max speed, temperature above TEMP_MAX or faulty
+      setPwmDuty(MAX_FAN_SPEED);
+      break;
   }
 
 }
 
 void setPwmDuty(byte duty) {
-  OCR1A = (word) (duty*TCNT1_TOP)/100;
+  OCR1A = (word) (duty * TCNT1_TOP) / 100;
 }
 
 byte calcPwmDuty(float temperature) {
-  // Linear 
+  // Linear
   float pwmDuty_float = k * temperature + m;
   // Convert to byte
-  byte pwmDuty_byte = (byte)(pwmDuty_float+0.5);
+  byte pwmDuty_byte = (byte)(pwmDuty_float + 0.5);
+
+  // Saurate calculated PWM duty. It should be no smaller than MIN_FAN_SPEED and not
+  // greater than MAX_FAN_SPEED.
+  pwmDuty_byte = max(pwmDuty_byte, MIN_FAN_SPEED);
+  pwmDuty_byte = min(pwmDuty_byte, MAX_FAN_SPEED);
+  
   return pwmDuty_byte;
 }
